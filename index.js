dc0a4e1273a55bf22c96fed070c9c96d52e36f52
Perla <pataditaenloshuevos@gmail.com>
2019-04-30 09:35:35 -0500

    se convirtio las funciones de ruta relativa a absuluta a promesa

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+// nombre de la variable igual que el nombre del modulo
+const fs = require('fs');
 const path = require('path');
+const marked = require('marked');
+const fetch = require('node-fetch');
 
-const pathAbsolute  = (pathEvaluate) => {  
-    return path.isAbsolute(pathEvaluate)
+// Funcion que convierte rutas relativas en absolutas con promesas.
+const pathAbsolute =(route)=>{
+  return new Promise(function(resolve, reject) {
+    resolve(file = path.resolve(route));
+  });
+}
+
+// Funcion leer archivo md
+const readFile=(file)=>{
+  return new Promise(function (resolve, reject) {
+    fs.readFile(file,function(error,md){
+      if(error){
+        return reject(error);
+      }
+      resolve(md,file);
+    });
+  });
 }
 
 
+
 module.exports = () => {
-  // ...
+  pathAbsolute,
+  readFile
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,30 +1,7 @@
-const pathAbsolute = require('../src/module/path.js');
+const {pathAbsolute, readFile} = require('../index.js');
 
-
-
-describe('pathAbsolute',()=>{
-  it('es una funcion',()=>{
-    expect(pathAbsolute.pathAbsolute).toBe('function');
+describe('pathAbsolute', () => {
+  it('shoudl be pathAbsolute a function', () => {
+    expect(typeof pathAbsolute).toBe('function');
   });
-});
-describe('pathAbsolute',()=>{
-  it ('deberia retornar true',()=>{
-    expect('pathAbsolute',(true)).tobe(true);
-  });
-});
-
-describe('pathAbsolute',()=>{
-  it ('deberia retornar false',()=>{
-    expect('pathAbsolute',(false)).tobe(false);
-  });
-});
-
-
-
-/*describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
-});*/
+})
