7985acce409f0be4ed9535f640af3d98fd08ab79
Perla <pataditaenloshuevos@gmail.com>
2019-05-06 13:12:17 -0500

    se hicieron los argumentos pero no funcionan

---
+++ cli.js
@@ -0,0 +1,25 @@
+const { mdLinks } = require ('./index.js');
+ const path = process.argv[2];
+ const oneOption = process.argv[3];
+ const twoOption = process.argv[4];
+
+
+ if( oneOption === '--validate' && twoOption === '--stats' ||  oneOption === '--stats' && twoOption === '--validate'){
+    mdLinks(path, {validate:true})
+    .then(res => {
+      statsOfLinks(res).then(res => console.log(res))
+      linksBroken(res).then(res => console.log(res))
+    })
+ } else if (oneOption === '--validate' || oneOption === '--v'){
+  mdLinks(path, {validate:true})
+  .then(res => { res.forEach( (objLinks) => { console.log(`file: ${objLinks.file}, \nhref: ${objLinks.href}, \nmessage:${objLinks.message}, \nstatus:${objLinks.status}, \ntext:${objLinks.text}`)})})
+  .catch(error => console.log(error))
+ } else if(oneOption === '--stats' || oneOption === '--s'){
+  mdLinks(path, {validate:false})
+  .then(res => {
+    statsOfLinks(res).then(res => console.log(res))
+  })
+ } else{
+  mdLinks(path, {validate:false})
+  .then(res => { res.forEach( (objLinks) => { console.log(`file: ${objLinks.file}, \nhref: ${objLinks.href}, \ntext:${objLinks.text}`)})})
+ }
--- index.js
+++ index.js
@@ -3,6 +3,139 @@ const path = require('path');// nombre de la variable igual que el nombre del mo
 const marked = require('marked');
 const fetch = require('node-fetch');
 
+//si la ruta es absoluta
+const pathAbsolute = (pathEvalue) => {           //booleano
+    return path.isAbsolute(pathEvalue)
+};
+//si la ruta es relativa.
+const relativePath = (pathRelative) => {
+    return path.resolve(pathRelative)
+};
+//evaluar si es un directorio
+const pathDirectory = (pathEvalue) => {
+    return fs.lstatSync(pathEvalue).isDirectory();
+ };
+
+const pathFile = (pathEvalue) => {
+    return fs.lstatSync(pathEvalue).isFile();
+ };
+
+const fileMD = (route) => {
+    return path.extname(route) === '.md';
+ };
+
+const contentFiles = (route) => {
+    let contentFileMD = [];
+     if (pathFile(route)) {
+        if (fileMD(route)){
+            contentFileMD.push(route)
+        }
+     } if (pathDirectory(route)){
+        let files = fs.readdirSync(route);   //trae el nombre de cada uno de los que está dentro del directorio
+        files.forEach((name) => {
+            contentFileMD = contentFileMD.concat(contentFiles(path.join(route, name)));
+        })
+     }
+     return contentFileMD;
+  };
+
+const getLinks = (arrayRoutesMD) => {
+    let arrayObjLinks = [];
+    arrayRoutesMD.forEach((routeMD) => {
+        const readFiles =  fs.readFileSync(routeMD, 'utf8');
+        const renderer = new marked.Renderer();
+        renderer.link = (href, __, text) => {
+            arrayObjLinks.push({ href, text, file: routeMD });
+            return '';
+      }
+      marked(readFiles, {renderer});
+    })
+    return arrayObjLinks;
+};
+
+const validateLinks = (arrayObjLinks) => {
+    const roveArrObj = arrayObjLinks.map((links) => {
+        return new Promise ((resolve)=>{
+         fetch(links.href)
+           .then(res => {
+             if (res.status >= 200 && res.status < 400) {
+               links.status = res.status;
+               links.message = 'OK';
+               resolve(links);
+             } else {
+               links.status = res.status;
+               links.message = 'Fail';
+               resolve(links);
+             }
+           }).catch(error => {
+            links.status = '';
+            links.message = 'Not Found';
+            resolve(links);
+           });
+       });
+   });
+   return Promise.all(roveArrObj);
+   };
+
+
+const statsOfLinks = (arrObjLinks) => {
+        const validate = validateLinks(arrObjLinks);
+        return new Promise ((resolve)=>{
+           validate.then((links)=>{
+               const allLinks = links.length;
+           const linksUniques = [...new Set(links.map(links => links.href))].length;
+           resolve(`Total: ${allLinks} Unico: ${linksUniques}`);
+        })
+      })
+   };
+
+const linksBroken = (arrObjLinks) => {
+       const validate = validateLinks(arrObjLinks);
+       return new Promise ((resolve)=>{
+          validate.then((link)=>{
+          const getBroken = link.filter(links => links.message === 'Fail');
+          const brokens = getBroken.length;
+          resolve(`Enlace roto: ${brokens}`);
+       })
+     })
+   };
+
+
+
+
+const mdLinks = (path, options) => {
+
+     let pathAbs;
+     if (!pathAbsolute(path)) {
+       pathAbs = relativeAbsolute(path);
+     } else {
+       pathAbs = path;
+     };
+     return new Promise((resolve) => {
+       if (!options.validate) {
+         resolve(getLinks(contentFiles(pathAbs)));
+       } if (options.validate) {
+         resolve(validateLinks(getLinks(contentFiles(pathAbs))));
+       }
+     })
+  };
+
+   module.exports = {
+     pathAbsolute,
+     relativePath,
+     pathDirectory,
+     pathFile,
+     fileMD,
+     contentFiles,
+     getLinks,
+     validateLinks,
+     statsOfLinks,
+     linksBroken,
+     mdLinks,
+     mdLinks
+   };
+
+/*
 // Funcion que convierte rutas relativas en absolutas
 const pathAbsolute=(route)=> {
   return new Promise(function(resolve, reject) {
@@ -87,3 +220,4 @@ module.exports = {
   convertMd,
   newUrl
 };
+*/
